291515b75926fb1e13eb337f07f7ddeca164ce31
MJ <michael.mj.john@gmail.com>
2018-12-16 20:10:51 -0800

    initial checkin
    note: using fastLED lib

---
+++ xmasTree_2018/lightHash.h
@@ -0,0 +1,36 @@
+
+// stores lights in row/column array. Hashes row/column to light index
+// taking into account vertical swaps and also masked lights @ bottom
+// of the tree
+
+#define ROWS 20
+#define COLUMNS 7
+
+uint8_t lights[COLUMNS][ROWS];
+
+void initLights() { 
+  int lightIndex = 0;
+  int currentJump = 0;
+ 
+  for( int i=0; i<COLUMNS; i++ ) {
+    if( i % 2 == 0 ) { //even numbered column
+      for( int j=0; j<ROWS; j++ ) {
+        lights[i][j] = lightIndex + currentJump;
+        lightIndex++; 
+      }
+    }
+    else {  //odd numbered column, has to count down
+      lightIndex += ROWS-1;
+      for( int j=0; j<ROWS; j++ ) {
+        lights[i][j] = lightIndex + currentJump;
+        lightIndex--; 
+      }
+      lightIndex += ROWS+1;      
+    }
+    // accommodate the lights that should be masked out at the bottom of the tree
+    if( lightIndex - currentJump == 40 ) { currentJump += 6; }
+    if( lightIndex - currentJump == 80 ) { currentJump += 5; }
+    if( lightIndex - currentJump == 120 ) { currentJump += 4; }
+  }
+  
+}
---
+++ xmasTree_2018/xmasTree_2018.ino
@@ -0,0 +1,61 @@
+#include <FastLED.h>
+#include "lightHash.h"
+  
+#define NUM_LEDS 150
+#define PIN 7
+
+CRGB leds[NUM_LEDS];
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+volatile byte mode=0;
+int snowFlakes[COLUMNS]; // will hold the value for each column that has one snowflake
+bool isBlue = false;
+
+
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
+
+  initLights();
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+ 
+     // make everything blue
+     if( !isBlue ) { 
+       for( int col=0; col<COLUMNS; col++ ){
+            for( int row=0; row<ROWS; row++ ) {
+              leds[lights[col][row]] = CRGB::Blue;
+            }
+            //ensure that snowflake flags are initialized to zero
+            snowFlakes[col] = -1; 
+            snowFlakes[col] = -1; 
+        }
+        isBlue = true;
+     }
+    
+    for( int row = ROWS-1; row >=-1; row-- ) {
+      for( int col = 0; col < COLUMNS; col++ ) {
+        if( row == -1 ) {
+          leds[lights[col][row+1]] = CRGB::Blue;
+            if( random(100) < 25 ) {
+               snowFlakes[col] = 19;
+            }
+        }
+        else if( snowFlakes[col] == row ) {
+            snowFlakes[col]--;
+            leds[lights[col][row]].setRGB( 50, 50, 50 );
+            if( row < 19 ) { leds[lights[col][row+1]] = CRGB::Blue; } // trailing back to blue
+        }
+      }
+      FastLED.show();
+      delay(100);
+    }    
+ }
+    
+  
