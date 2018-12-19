98dffed7f3c41ddd546c24fc0315a4ecd4f3f025
MJ <michael.mj.john@gmail.com>
2018-12-18 16:19:09 -0800

    upload fails.
    Restructured (simplified) the project into fewer code files. Tried to
    test but it won't upload.

--- xmasTree_2018/xmasTree_2018.ino
+++ xmasTree_2018/xmasTree_2018.ino
@@ -4,11 +4,12 @@
 
 #include <FastLED.h>
 #include "lightHash.h"
-#include "Snowflake.h"
-#include "noisefunctions.h"
+//#include "Snowflake.h"
+#include "noisefunctions.cpp"
   
 #define PIN 7
 #define FLAKEMAX 50
+#define FIRE true
 
   
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
@@ -16,20 +17,91 @@
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
+#ifdef SNOWFLAKE
 bool isBlue = false;
 struct Snowflake snowflakes[FLAKEMAX];
 uint8_t flakeCount = 0;
-uint32_t lastSpawn; uint32_t minDelay;
+uint32_t lastSpawn; uint32_t minDelay=300;
+#endif
+
+#ifdef FIRE
+const uint8_t kMatrixWidth = 7;
+const uint8_t kMatrixHeight = 20;
+#define MAX_DIMENSION ((kMatrixWidth>kMatrixHeight) ? kMatrixWidth : kMatrixHeight)
+#define NUM_LEDS_ACTIVE = kMatrixWidth * kMatrixHeight
+
+static uint16_t noiseX;  // column
+static uint16_t noiseY;  // row
+static uint16_t noiseZ;  // time
+
+uint16_t noiseSpeed = 20;
+uint16_t scale = 311;  // starting point. Scales up from 1 to about 5K (for distance between pixels)
+
+// This is the array that we keep our computed noise values in
+uint8_t noise[MAX_DIMENSION][MAX_DIMENSION];
+#endif
+
 
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
   initLights();
-  minDelay = 300;
   randomSeed(analogRead(0));
 }
 
 void loop() {
+
+    #ifdef SNOWFLAKE
+    snowflakeUpdate();
+    #endif
+
+    #ifdef FIRE
+    fireUpdate();
+    #endif
+  
+    FastLED.show();
+ 
+ }
+
+
+void fireUpdate () {
+
+  fillNoise8();
+  renderNoise();
+
+  
+}
+
+// Fill the x/y array of 8-bit noise values using the inoise8 function.
+void fillNoise8() {
+  for(int i = 0; i < MAX_DIMENSION; i++) {
+    int ioffset = scale * i;
+    for(int j = 0; j < MAX_DIMENSION; j++) {
+      int joffset = scale * j;
+      noise[i][j] = inoise8(noiseX + ioffset,noiseY + joffset,noiseZ);
+    }
+  }
+  noiseZ += noiseSpeed;
+}
+
+void renderNoise() {
+  for(int i = 0; i < kMatrixWidth; i++) {
+    for(int j = 0; j < kMatrixHeight; j++) {
+      // We use the value at the (i,j) coordinate in the noise
+      // array for our brightness, and the flipped value from (j,i)
+      // for our pixel's hue.
+
+      leds[lights[i][j]] = CHSV(noise[j][i],255,noise[i][j]);
+
+      // You can also explore other ways to constrain the hue used, like below
+      // leds[XY(i,j)] = CHSV(ihue + (noise[j][i]>>2),255,noise[i][j]);
+    }
+  }
+}
+
+
+#ifdef SNOWFLAKE
+void snowflakeUpdate() {
      
      // make everything blue
      if( !isBlue ) { 
@@ -54,10 +126,7 @@ void loop() {
      updateFlakes();
 
      Serial.println(flakeCount);
-     FastLED.show();
- 
- }
-
+}
 
  void updateFlakes() {
 
@@ -81,3 +150,4 @@ boolean addFlake( void ) {
         }
         return false;
 }
+#endif
